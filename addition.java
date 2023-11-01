11f188567abcacd0788e22f05488e75fd15158bc
aryaparasharmrt <66545234+aryaparasharmrt@users.noreply.github.com>
2023-11-01 11:47:21 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -5,7 +5,9 @@ public static void main (String args[]){
   Scanner sc = new Scanner(System.in);
   int a = sc.nextInt();
   int b = sc.nextInt();
-  int result = a + b;
+  int result = a - b;
 
   System.out.print(result);
 }  
+
+
